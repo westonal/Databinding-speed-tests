36ce5357fce9773908dec6d791fd9ff09250a610
Alan Evans <thealanevans@gmail.com>
2018-03-13 13:36:15 -0300

    10 VMs, 10 shared layouts in main

---
+++ app/src/atBindable/java/com/example/VM10.kt
@@ -0,0 +1,78 @@
+package com.example
+
+import android.databinding.BaseObservable
+import android.databinding.Bindable
+import example.com.databindingspeed.BR
+
+class VM10 : BaseObservable() {
+
+    @Bindable
+    var a: String = ""
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.a)
+        }
+
+    @Bindable
+    var b: String = ""
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.b)
+        }
+
+    @Bindable
+    var c: String = ""
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.c)
+        }
+
+    @Bindable
+    var d: String = ""
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.d)
+        }
+
+    @Bindable
+    var e: String = ""
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.e)
+        }
+
+    @Bindable
+    var f: String = ""
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.f)
+        }
+
+    @Bindable
+    var g: String = ""
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.g)
+        }
+
+    @Bindable
+    var h: String = ""
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.h)
+        }
+
+    @Bindable
+    var i: String = ""
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.i)
+        }
+
+    @Bindable
+    var j: String = ""
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.j)
+        }
+}
---
+++ app/src/observableFields/java/com/example/VM4.kt
@@ -0,0 +1,16 @@
+package com.example
+
+import android.databinding.ObservableField
+
+class VM4 {
+    val a = ObservableField<String>()
+    val b = ObservableField<String>()
+    val c = ObservableField<String>()
+    val d = ObservableField<String>()
+    val e = ObservableField<String>()
+    val f = ObservableField<String>()
+    val g = ObservableField<String>()
+    val h = ObservableField<String>()
+    val i = ObservableField<String>()
+    val j = ObservableField<String>()
+}
